75eeacecfa49a90a6d1fa4d1e3437c6633ae10ec
artyommm <bahareff.artyom@yandex.ru>
2023-04-09 22:28:45 +0500

    Добавил 1 и 2 лабы

---
+++ test.py
@@ -0,0 +1,21 @@
+# from lab_1 import lab1, laba_1
+from lab_2 import lab2
+
+# print(lab1.find_nod(54, 89))
+
+# print(sorted(list(lab1.prime(293))))
+
+# print(lab1.eiler_func(4))
+
+# print(lab1.solve_linear_congruence(11, 17, 119))
+
+# print(lab1.solve_linear_congruence(5, 4, 21))
+
+# print(lab1.solve_linear_congruence_2(12, 8, 67))
+
+# print(lab1.primfacs(1132))
+
+# lab2.shifr_table('ПРИХОДИТЕ_ЗАВТРА:)kekekeketkekrerk')
+# laba_1.line_compare(11, 17, 119)
+
+lab2.RSA(15, 23, 37, 47)
